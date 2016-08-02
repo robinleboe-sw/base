e60811b876bcf20dc6b4a917b20ffd56b4eef915
Robin Leboe <robin@sessionwire.com>
2016-08-02 16:15:00 -0700

    updated login page

--- imports/ui/pages/login.js
+++ imports/ui/pages/login.js
@@ -13,34 +13,67 @@ export class Login extends React.Component {
   }
 
   render() {
-    return <Row>
-      <Col xs={ 12 } sm={ 6 } md={ 4 }>
-        <h4 className="page-header">Login</h4>
-        <form ref="login" className="login" onSubmit={ this.handleSubmit }>
-          <FormGroup>
-            <ControlLabel>Email Address</ControlLabel>
-            <FormControl
-              type="email"
-              ref="emailAddress"
-              name="emailAddress"
-              placeholder="Email Address"
-            />
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>
-              <span className="pull-left">Password</span>
-              <Link className="pull-right" to="/recover-password">Forgot Password?</Link>
-            </ControlLabel>
-            <FormControl
-              type="password"
-              ref="password"
-              name="password"
-              placeholder="Password"
-            />
-          </FormGroup>
-          <Button type="submit" bsStyle="success">Login</Button>
-        </form>
-      </Col>
-    </Row>;
+    return (
+
+    <div className="loginColumns animated fadeInDown">
+      <div className="row">
+
+        <div className="col-md-6">
+          <h2 className="font-bold">Welcome to Sessionwire Studio</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et orci mollis nibh placerat venenatis. Proin sollicitudin ac dui et accumsan. Sed vel justo odio.
+          </p>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+          </p>
+
+          <p>
+            When an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+
+        </div>
+        <div className="col-md-6">
+          <div className="ibox-content">
+            <form ref="login" className="login" onSubmit={ this.handleSubmit }>
+              <FormGroup>
+                <FormControl
+                  type="email"
+                  ref="emailAddress"
+                  name="emailAddress"
+                  placeholder="Email Address"
+                />
+              </FormGroup>
+              <FormGroup>
+                <FormControl
+                  type="password"
+                  ref="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <ControlLabel>
+                  <Link className="pull-right" to="/recover-password">Forgot Password?</Link>
+                </ControlLabel>
+              </FormGroup>
+              <Button type="submit" bsStyle="btn btn-primary block full-width m-b">Login</Button>
+            </form>
+            <p className="m-t">
+              <small>Sessionwire Studio - Live recording on the Web © 2016</small>
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr/>
+        <div className="row">
+          <div className="col-md-6">
+            Copyright Sessionwire Communications Inc.
+          </div>
+          <div className="col-md-6 text-right">
+            <small>© 2013-2016</small>
+          </div>
+        </div>
+    </div>
+  )
   }
 }
