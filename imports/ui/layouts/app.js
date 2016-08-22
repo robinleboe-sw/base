d845c300e9270ed8008433fc8628d027a7340258
Robin Leboe <robin@sessionwire.com>
2016-08-22 10:21:54 -0700

    update app.js to use ES6 class

--- imports/ui/layouts/app.js
+++ imports/ui/layouts/app.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Grid } from 'react-bootstrap';
-import AppNavigation from '../containers/app-navigation';
+/**
+ * Main application page for Sessionwire Studio. Application-wide navigation is rendered here and the App component's
+ * children are rendered into a Bootstrap grid courtesy of React Bootstrap.
+ */
 
-export const App = React.createClass({
-  propTypes: {
-    children: React.PropTypes.element.isRequired,
-  },
+import React from 'react'
+import { Grid } from 'react-bootstrap'
+import AppNavigation from '../containers/app-navigation'
+
+export class App extends React.Component {
+  constructor(props) {
+    // call super to use 'this.'
+    super(props)
+  }
+
   render() {
-    return <div>
-      <AppNavigation />
-      <Grid>
-        { this.props.children }
-      </Grid>
-    </div>;
-  },
-});
+    return(
+      <div>
+        <AppNavigation />
+        <Grid>
+          { this.props.children }
+        </Grid>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  children: React.PropTypes.element.isRequired
+}
